a9bc4eb68f3e65349fe8b0fa03683262c59c2e11
Dan <daniel9.bennett@live.uwe.ac.uk>
2019-09-25 15:47:53 +0100

    Start of item system
    Going to look into a proper dictionary system and player collision to pick up items.

---
+++ New_Honk_City/Assets/Scripts/ItemBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehaviour : MonoBehaviour
+{
+    public Items data;        
+    
+    public string ItemName;
+    public string NPCName;
+
+    public bool honk = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        data = GetComponent<Items>();
+        honk = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if (honk == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                honk = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        honk = true;
+    }
+
+
+}
---
+++ New_Honk_City/Assets/Scripts/Items.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    
+    Dictionary<string, string> NPC = new Dictionary<string, string>();
+    public Dictionary<string, string> Item = new Dictionary<string, string>();
+
+    private void Start()
+    {
+        Item.Add("Key", "NPC1");
+        Item.Add("Rake", "NPC1");
+        Item.Add("", "");
+        Item.Add("", "");
+        Item.Add("", "");
+        Item.Add("", "");
+        Item.Add("", "");
+        Item.Add("", "");
+    }
+
+    private void Update()
+    {
+        foreach(KeyValuePair<string, string> keyValue in Item)
+        {
+
+        }
+    }
+
+}
