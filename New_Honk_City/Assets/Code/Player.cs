a62b991308969343aed4d4c08ccf592ba2199e2a
Joel Draper <jepdraper@gmail.com>
2019-09-25 14:50:53 +0100

    Added New Input System
    Added new input system 
    Tried implementing new input system

---
+++ New_Honk_City/Assets/Code/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour, GeneralInputs.IPlayerActions
+{
+    private GeneralInputs controls;
+
+    void Awake()
+    {
+        controls = new GeneralInputs();
+        controls.Player.SetCallbacks(this);
+    }
+
+    void OnEnable()
+    {
+        controls.Player.Enable();
+        Debug.Log("<color=green>Enabled controller</color>");
+    }
+
+    void OnDisable()
+    {
+        controls.Player.Disable();
+        Debug.Log("<color=green>Disabled controller</color>");
+    }
+
+    public void OnHonk(InputAction.CallbackContext context)
+    {
+        Debug.Log("<color=red>HONK!</color>");
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+}
