432946b438d2f3e270381dd5ada7fa3803e9a13a
D25-Bennett <daniel9.bennett@live.uwe.ac.uk>
2019-09-27 21:25:14 +0100

    Items work better
    Goose can pick up and drop items without issue. When moving left or right the item flips and moves to the correct side. 
    Main issue is dealing with multiple items. Goose is greedy.

--- New_Honk_City/Assets/Scripts/ItemBehaviour.cs
+++ New_Honk_City/Assets/Scripts/ItemBehaviour.cs
@@ -6,57 +6,76 @@ public class ItemBehaviour : MonoBehaviour
 {
     public bool honk = false;
     public bool cooldown = false;
-    public float time;
+    [SerializeField] private GameObject beak = null;
+    [SerializeField] private GameObject hand = null;
+
+    
+    //public Player player = null;
 
     void Start()
     {        
         honk = false;
         cooldown = false;
+        //player = GetComponent<Player>();
     }
 
     void Update()
     {
-     if (honk == true)
+        if (honk == true)
         {
+            if (Input.GetKey(KeyCode.A))
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 this.transform.parent = null;
-                time = 1.5f;
-                CountDown();
+                Debug.Log("Item Dropped");
                 honk = false;
+                cooldown = true;
             }
-        }
-        timeCheck();
+        }        
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (cooldown == false)
+        if (cooldown == false) // && player.itemHeld == false)
         {
             if (other.gameObject.tag == "Player")
-            {            
+            {
                 honk = true;
-                this.transform.parent = other.transform;
+                this.transform.parent = beak.transform;
+                transform.localPosition = new Vector2(0,0);
             }
+            
         }
+        if (other.gameObject.tag == "NPC")
+        {
+            if (honk == true)
+            {
+                this.transform.parent = null;
+                this.transform.parent = hand.transform;            
+            }
+            else
+            {
+                this.transform.parent = hand.transform;
+                honk = false;
+                cooldown = false;
+            }
+            transform.localPosition = new Vector2(0, 0);
+        }
+
     }
 
-    void CountDown()
+    private void OnTriggerExit2D(Collider2D other)
     {
-        time -= Time.deltaTime;
-        cooldown = true;
+        cooldown = false;
     }
 
-    void timeCheck()
-    {
-        if (time >= 0)
-        {
-            cooldown = false;
-        }
-        else if (time <= 0)
-        {
-            cooldown = true;
-        }
-    }
-//http://docs.unity3d.com/Documentation/ScriptReference/Collider.OnTriggerEnter.html?_ga=2.99166366.1045234588.1569588832-344143463.1569588832
+    //http://docs.unity3d.com/Documentation/ScriptReference/Collider.OnTriggerEnter.html?_ga=2.99166366.1045234588.1569588832-344143463.1569588832
 }
