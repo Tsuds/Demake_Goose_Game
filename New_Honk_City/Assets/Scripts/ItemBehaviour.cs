5ee908d167e7b16d9e31ebeb5a1ab1414a919d2e
D25-Bennett <daniel9.bennett@live.uwe.ac.uk>
2019-09-28 19:17:39 +0100

    Items can now be held one at a time only
    The goose is now more understanding is taking only one object at a time and understands when to drop item.
    Waiting to fully implement NPC handling of items.

--- New_Honk_City/Assets/Scripts/ItemBehaviour.cs
+++ New_Honk_City/Assets/Scripts/ItemBehaviour.cs
@@ -4,24 +4,19 @@ using UnityEngine;
 
 public class ItemBehaviour : MonoBehaviour
 {
-    public bool honk = false;
-    public bool cooldown = false;
     [SerializeField] private GameObject beak = null;
     [SerializeField] private GameObject hand = null;
-
-    
-    //public Player player = null;
+    public ItemMaster items;
+    public bool cooldown = false;
 
     void Start()
-    {        
-        honk = false;
+    {
         cooldown = false;
-        //player = GetComponent<Player>();
     }
 
     void Update()
     {
-        if (honk == true)
+        if (transform.parent == true)//(items.ItemHeld == true)
         {
             if (Input.GetKey(KeyCode.A))
             {
@@ -36,46 +31,45 @@ public class ItemBehaviour : MonoBehaviour
             {
                 this.transform.parent = null;
                 Debug.Log("Item Dropped");
-                honk = false;
                 cooldown = true;
+                items.ItemHeld = false;
             }
-        }        
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (cooldown == false) // && player.itemHeld == false)
+        // Checks if an item is already held by player
+        if (cooldown == false //&& transform.parent == false
+            && items.ItemHeld == false && items.NPCHeld == false)
         {
             if (other.gameObject.tag == "Player")
             {
-                honk = true;
                 this.transform.parent = beak.transform;
-                transform.localPosition = new Vector2(0,0);
+                transform.localPosition = new Vector2(0, 0);
+                items.ItemHeld = true;
             }
-            
         }
+
         if (other.gameObject.tag == "NPC")
         {
-            if (honk == true)
+            if (items.ItemHeld == false)
             {
                 this.transform.parent = null;
-                this.transform.parent = hand.transform;            
+                this.transform.parent = hand.transform;
             }
             else
             {
                 this.transform.parent = hand.transform;
-                honk = false;
                 cooldown = false;
+                items.ItemHeld = false;
             }
             transform.localPosition = new Vector2(0, 0);
         }
-
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         cooldown = false;
     }
-
-    //http://docs.unity3d.com/Documentation/ScriptReference/Collider.OnTriggerEnter.html?_ga=2.99166366.1045234588.1569588832-344143463.1569588832
 }
