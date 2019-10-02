9645e353ff710037f1608a9adbfd9b2a424ce022
Unknown <Drew2.Mayhew@live.uwe.ac.uk>
2019-10-02 09:50:06 +0100

    Smoothing Removed, Colliders Added

--- New_Honk_City/Assets/CameraFollow.cs
+++ New_Honk_City/Assets/CameraFollow.cs
@@ -24,14 +24,14 @@ public class CameraFollow : MonoBehaviour
         Vector3 cameraFollowPosition = GetCameraFollowPositionFunc();
         cameraFollowPosition.z = transform.position.z;
 
-        Vector3 cameraMoveDir = (cameraFollowPosition - transform.position).normalized;
+        //Vector3 cameraMoveDir = (cameraFollowPosition - transform.position).normalized;
         float distance = Vector3.Distance(cameraFollowPosition, transform.position);
         float cameraMoveSpeed = 1f;
 
         //Prevents Low FPS Overshoot
         if(distance > 0)
         {
-            Vector3 newCameraPosition = transform.position + cameraMoveDir * distance * cameraMoveSpeed * Time.deltaTime;
+            Vector3 newCameraPosition = transform.position /*+ cameraMoveDir */ * distance * cameraMoveSpeed * Time.deltaTime;
             float distanceAfterMoving = Vector3.Distance(newCameraPosition, cameraFollowPosition);
 
             if (distanceAfterMoving > distance)
