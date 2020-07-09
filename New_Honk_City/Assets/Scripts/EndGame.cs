5b4caa478db3bcdccd37e099d4cfd2e1ac69709b
Drew <Drew2.Mayhew@live.uwe.ac.uk>
2020-07-09 17:06:35 +0100

    End Screen Added, Scenes Removed

---
+++ New_Honk_City/Assets/Scripts/EndGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    private bool gameOver = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            gameOver = true;  
+            SceneManager.LoadScene(1);
+        }
+    }
+}
